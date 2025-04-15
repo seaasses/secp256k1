9c0ce3fe28706f7b783391c5d408fa7b67e96035
seaasses <marcusvglac@gmail.com>
2025-04-14 23:03:15 -0300

    update assert_uint256_equal

--- src/unity/unity_extension.c
+++ src/unity/unity_extension.c
@@ -1,7 +1,14 @@
 #include "structs/uint256.h"
 #include "unity/unity.h"
+#include <stdio.h>
 
 void assert_uint256_equal(Uint256 expected, Uint256 actual)
 {
-    TEST_ASSERT_EQUAL_UINT64_ARRAY(expected.limbs, actual.limbs, 4);
+
+    char message[100];
+    for (int i = 0; i != 3; ++i)
+    {
+        sprintf(message, "Limb %d error", i);
+        TEST_ASSERT_EQUAL_UINT64_MESSAGE(expected.limbs[i], actual.limbs[i], message);
+    }
 }
