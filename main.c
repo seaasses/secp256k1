d2dd7a093a6de98f8cf1b0296c8e6449d6512b12
seaasses <marcusvglac@gmail.com>
2025-04-14 23:03:52 -0300

    add tests to main

--- main.c
+++ main.c
@@ -1,21 +1,30 @@
-#include "structs/uint256.h"
-#include "uint256/shift_left.h"
-#include "uint256/print_uint256.h"
+#include "unity/unity.h"
+#include "tests/uint256/shift_left.c"
 
 #include <stdio.h>
 #include <stdint.h>
 
+int main(void)
+{
+    UNITY_BEGIN();
 
-int main() {
-    UInt256 a = (UInt256) {
-        .limbs = {
-            0,0,0,1
-        }
-    };
+    RUN_TEST(test_shift_left_0);
+    RUN_TEST(test_shift_left_1);
+    RUN_TEST(test_shift_left_between_limbs_3_2);
+    RUN_TEST(test_shift_left_between_limbs_2_1);
+    RUN_TEST(test_shift_left_between_limbs_1_0);
+    RUN_TEST(test_shift_left_lose_more_significant_bit);
+    RUN_TEST(test_shift_left_all_1s);
+    RUN_TEST(test_shift_left_0_1_bits_pattern);
+    RUN_TEST(test_shift_left_1_0_bits_pattern);
+    RUN_TEST(test_shift_left_0_ff_bytes_pattern);
+    RUN_TEST(test_shift_left_ff_00_bytes_pattern);
+    RUN_TEST(test_shift_left_ffffffffffffffff_0000000000000000_limbs_pattern);
+    RUN_TEST(test_shift_left_0000000000000000_ffffffffffffffff_limbs_pattern);
+    RUN_TEST(test_shift_left_bit_inside_byte);
+    RUN_TEST(test_shift_left_big_number);
 
-    UInt256 result;
 
-    uint256_shift_left(&a, &result);
-    print_uint256(result);
 
+    return UNITY_END();
 }
