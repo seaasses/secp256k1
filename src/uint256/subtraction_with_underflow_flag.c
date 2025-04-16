baa3e997b3533581f40c86fe9ca27057f33fc11f
seaasses <marcusvglac@gmail.com>
2025-04-16 18:35:28 -0300

    fix bitwise and warning

--- src/uint256/subtraction_with_underflow_flag.c
+++ src/uint256/subtraction_with_underflow_flag.c
@@ -10,10 +10,10 @@ inline void uint256_subtraction_with_underflow_flag(const Uint256 *a, const Uint
     unsigned int borrow = (a->limbs[3] < b->limbs[3]);
 
     result->limbs[2] = a->limbs[2] - b->limbs[2] - borrow;
-    borrow = (a->limbs[2] < b->limbs[2]) | (a->limbs[2] == b->limbs[2]) & borrow;
+    borrow = (a->limbs[2] < b->limbs[2]) | ((a->limbs[2] == b->limbs[2]) & borrow);
 
     result->limbs[1] = a->limbs[1] - b->limbs[1] - borrow;
-    borrow = (a->limbs[1] < b->limbs[1]) | (a->limbs[1] == b->limbs[1]) & borrow;
+    borrow = (a->limbs[1] < b->limbs[1]) | ((a->limbs[1] == b->limbs[1]) & borrow);
 
     result->limbs[0] = a->limbs[0] - b->limbs[0] - borrow;
 
