5b3211cad43aa6dca9b8ff4b7961d206476bc108
seaasses <marcusvglac@gmail.com>
2025-04-22 21:09:40 -0300

    use addition chain on montgomery inverse

--- src/montgomery/montgomery_inverse.c
+++ src/montgomery/montgomery_inverse.c
@@ -18,39 +18,280 @@ void montgomery_inverse(const Uint256 *a_mont, Uint256 *a_inv_mont)
 
     This uses Fermat's Little Theorem to compute the modular inverse.
 
+    To get a_mount ^ (N-2) mod N we use the same addition chain used by peterdettman in 2014
+    (https://github.com/bitcoin-core/secp256k1/commit/f8ccd9befdb22824ef9a845a90e3db57c1307c11)
 */
-    // TODO: Since the exponent (N-2) is constant and known at compile time, we can avoid dynamically testing each bit and instead perform the binary exponentiation directly.
 
-    Uint256 exponent = MONTGOMERY_N_MINUS_2;
-    Uint256 to_multiply;
+    Uint256 tmp_0, tmp_1, tmp_2, tmp_3, tmp_4;
 
-    unsigned long long limb;
-    unsigned long long to_multiply_mask;
-
-    *a_inv_mont = (Uint256)ONE_IN_MONTGOMERY;
-
-#pragma unroll
-    for (unsigned int i = 0; i < 4; ++i)
-    {
-        limb = exponent.limbs[i];
-#pragma unroll
-        for (unsigned int j = 0; j < 64; ++j)
-        {
-
-            montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
-            to_multiply_mask = -((limb & 0x8000000000000000) >> 63);
-            to_multiply = (Uint256){
-                .limbs = {
-                    a_mont->limbs[0] & to_multiply_mask,
-                    a_mont->limbs[1] & to_multiply_mask,
-                    (a_mont->limbs[2] & to_multiply_mask),
-                    (a_mont->limbs[3] & to_multiply_mask) | ((~to_multiply_mask) & ONE_IN_MONTGOMERY_3)
-
-                }};
-
-            montgomery_multiplication(a_inv_mont, &to_multiply, a_inv_mont);
-
-            limb <<= 1;
-        }
-    }
+    montgomery_multiplication(a_mont, a_mont, &tmp_0);
+    montgomery_multiplication(&tmp_0, a_mont, &tmp_1);
+    montgomery_multiplication(&tmp_1, &tmp_1, &tmp_0);
+    montgomery_multiplication(&tmp_0, a_mont, &tmp_2);
+    montgomery_multiplication(&tmp_2, &tmp_2, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_2, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_2, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_1, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_3, &tmp_0);
+    montgomery_multiplication(&tmp_0, &tmp_0, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_0, &tmp_3);
+    montgomery_multiplication(&tmp_3, &tmp_3, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_3, &tmp_4);
+    montgomery_multiplication(&tmp_4, &tmp_4, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, &tmp_4, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, &tmp_3, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, &tmp_2, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, &tmp_0, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, &tmp_1, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_inv_mont, a_inv_mont);
+    montgomery_multiplication(a_inv_mont, a_mont, a_inv_mont);
 }
